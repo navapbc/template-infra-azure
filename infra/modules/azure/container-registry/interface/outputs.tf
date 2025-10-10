dfef62f4552e7f4c862b84ec00ae3dbc1aad6c95
Jacob Pack <jacobpack@navapbc.com>
2025-10-10 15:36:08 -0400

    Updated container registry name substring

--- infra/modules/azure/container-registry/interface/outputs.tf
+++ infra/modules/azure/container-registry/interface/outputs.tf
@@ -1,7 +1,7 @@
 output "container_registry_name" {
   # image registry name must be globally unique, only allows alpha numeric
   # characters, and must be between 5 and 50 characters
-  value = substr(replace("${var.project_config.project_name}-${var.project_config.project_unique_id}", "/[^a-zA-Z0-9]/", ""), 0, 50)
+  value = substr(replace("${var.project_config.project_name}-${var.project_config.project_unique_id}", "/[^a-zA-Z0-9]/", ""), 0, 49)
 }
 
 output "container_registry_resource_group_name" {
