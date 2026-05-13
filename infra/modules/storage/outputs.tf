c7946028d2cabf603a91710edb800635b21a0b8f
Sean Thomas <sean.thomas@navapbc.com>
2026-05-13 17:53:02 -0400

    Add Event Grid System Topic for storage events
    - Create Event Grid System Topic scoped to storage account
    - Enables event-driven architecture similar to AWS EventBridge for S3
    - Add output for Event Grid topic ID for subscription management
    - Allows subscribers to receive blob created, deleted, and other storage events

--- infra/modules/storage/outputs.tf
+++ infra/modules/storage/outputs.tf
@@ -9,3 +9,8 @@ output "storage_account_name" {
 output "container_name" {
   value = azurerm_storage_container.documents.name
 }
+
+output "eventgrid_system_topic_id" {
+  description = "The ID of the Event Grid System Topic for storage events. Use this to create event subscriptions."
+  value       = azurerm_eventgrid_system_topic.storage.id
+}
