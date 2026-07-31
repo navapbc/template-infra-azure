94fbe8d07c916b546f0543536aa16fa192798f44
Tanner Doshier <git@doshitan.com>
2026-07-31 14:05:52 -0400

    Switch private endpoints to be placed in the subnet's resource group by default
    And use some clearer variable names. As well as dropping the target
    resource override input vars, still with just `resource_id` until we
    encounter a concrete case which can't use that.

--- infra/modules/azure/network/private-endpoint/main.tf
+++ infra/modules/azure/network/private-endpoint/main.tf
@@ -16,62 +16,53 @@ locals {
     "vaults" : ["vault"]
   }
 
-  parsed_resource_info = provider::azurerm::parse_resource_id(var.resource_id)
-  resource_type        = local.parsed_resource_info["resource_type"]
+  target_resource_info     = provider::azurerm::parse_resource_id(var.resource_id)
+  target_resource_type     = local.target_resource_info["resource_type"]
+  target_resource_name     = local.target_resource_info["resource_name"]
+  target_resource_provider = local.target_resource_info["resource_provider"]
 
-  subresource_names = var.subresource_names != null ? var.subresource_names : try(local.default_service_subresource_names[local.resource_type], [])
+  subresource_names = var.subresource_names != null ? var.subresource_names : try(local.default_service_subresource_names[local.target_resource_type], [])
 
-  resource_name       = var.resource_name != null ? var.resource_name : local.parsed_resource_info["resource_name"]
-  resource_group_name = var.resource_group_name != null ? var.resource_group_name : local.parsed_resource_info["resource_group_name"]
-  resource_location   = var.resource_location != null ? var.resource_location : try(data.azurerm_resource_group.rg[0].location, null)
+  subnet_info    = provider::azurerm::parse_resource_id(var.subnet_id)
+  subnet_rg_name = local.subnet_info["resource_group_name"]
 
-  parsed_subnet_info        = var.enable ? provider::azurerm::parse_resource_id(var.subnet_id) : null
-  private_endpoint_location = var.enable ? data.azurerm_resource_group.subnet_rg[0].location : null
-  parsed_subnet_rg_name     = var.enable ? local.parsed_subnet_info["resource_group_name"] : null
+  private_endpoint_name     = var.name != null ? var.name : substr(local.target_resource_info["resource_name"], 0, 64)
+  private_endpoint_rg_name  = var.resource_group_name != null ? var.resource_group_name : local.subnet_rg_name
+  private_endpoint_location = data.azurerm_resource_group.subnet_rg.location
 
-  dns_zone_name = var.dns_zone_key != null ? module.endpoint_refs.zones[var.dns_zone_key] : module.endpoint_refs.zones_by_resource_type[local.resource_type]
+  dns_zone_name = var.dns_zone_key != null ? module.endpoint_refs.zones[var.dns_zone_key] : try(module.endpoint_refs.zones_by_provider[local.target_resource_provider], module.endpoint_refs.zones_by_type[local.target_resource_type])
 }
 
 module "endpoint_refs" {
   source = "../../private-endpoint-dns-refs"
 }
 
-data "azurerm_resource_group" "rg" {
-  count = var.resource_location == null ? 1 : 0
-
-  name = local.parsed_resource_info["resource_group_name"]
-}
-
 data "azurerm_resource_group" "subnet_rg" {
-  count = var.enable ? 1 : 0
-
-  name = local.parsed_subnet_rg_name
+  name = local.subnet_rg_name
 }
 
 data "azurerm_private_dns_zone" "service_zone" {
-  count = var.enable ? 1 : 0
-
   name                = local.dns_zone_name
-  resource_group_name = local.parsed_subnet_rg_name
+  resource_group_name = local.subnet_rg_name
 }
 
 resource "azurerm_private_endpoint" "service" {
   count = var.enable ? 1 : 0
 
-  name                = local.resource_name
+  name                = local.private_endpoint_name
   location            = local.private_endpoint_location
-  resource_group_name = local.resource_group_name
+  resource_group_name = local.private_endpoint_rg_name
   subnet_id           = var.subnet_id
 
   private_service_connection {
-    name                           = local.resource_name
+    name                           = local.target_resource_name
     private_connection_resource_id = var.resource_id
     subresource_names              = local.subresource_names
     is_manual_connection           = false
   }
 
   private_dns_zone_group {
-    name                 = local.resource_name
-    private_dns_zone_ids = [data.azurerm_private_dns_zone.service_zone[0].id]
+    name                 = local.target_resource_name
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.service_zone.id]
   }
 }
--- infra/modules/azure/network/private-endpoint/variables.tf
+++ infra/modules/azure/network/private-endpoint/variables.tf
@@ -3,11 +3,6 @@ variable "enable" {
   default = true
 }
 
-variable "dns_zone_key" {
-  type    = string
-  default = null
-}
-
 variable "subnet_id" {
   type = string
 }
@@ -16,22 +11,24 @@ variable "resource_id" {
   type = string
 }
 
-variable "resource_name" {
-  type    = string
-  default = null
-}
-
-variable "resource_location" {
-  type    = string
-  default = null
-}
-
-variable "resource_group_name" {
-  type    = string
-  default = null
-}
-
 variable "subresource_names" {
   type    = list(string)
   default = null
+}
+
+variable "dns_zone_key" {
+  type    = string
+  default = null
+}
+
+variable "name" {
+  type        = string
+  default     = null
+  description = "Name for the Private Endpoint resource. If not specified, will use the name of the targeted resource."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = null
+  description = "Resource Group for the Private Endpoint resource. If not specified, will use the resource group of the subnet."
 }
